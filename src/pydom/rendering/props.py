bccad23de6fa745a4dddad5798ffe0de27b64f82
Neriya Cohen <ee10293847@gmail.com>
2024-09-12 22:39:59 +0300

    Remove conditional update since falsy transformer exists

--- src/pydom/rendering/props.py
+++ src/pydom/rendering/props.py
@@ -27,13 +27,7 @@ def transform_props(element: "ContextNode", *, context: "Context"):
                 f"Invalid matcher: {matcher} must be a callable or a string."
             )
 
-    element.props.update(
-        {
-            key: value
-            for key, value in element.props.items()
-            if value not in (False, None)
-        }
-    )
+    element.props.update(element.props)
 
 
 def render_props(props: Dict[str, Any]) -> str:
